564ce4ff4da6626c9c01de812310d0c55e14a6a1
Cade <cade@hyperblaster.org>
2015-03-30 12:42:59 +1100

    Fluro Config Tests

--- dist/fluro-config.js
+++ dist/fluro-config.js
@@ -1,20 +1,3 @@
-angular.module('fluro.config')
-
-
-.factory('FluroAuthentication', function($q, Fluro) {
-    return {
-        'request': function(config) {
-            if (Fluro.token) {
-                console.log('Add API KEY', Fluro.token)
-                config.headers.Authorization = 'Bearer ' + Fluro.token;
-            }
-            return config;
-        },
-    };
-});
-
-
-
 //////////////////////////////////////////
 
 //Create Fluro Config
@@ -41,6 +24,23 @@ angular.module('fluro.config', [])
         }
     };
 })
+
+
+
+
+.service('FluroAuthentication', function($q, Fluro) {
+    return {
+        'request': function(config) {
+            if (Fluro.token) {
+                console.log('Add API KEY', Fluro.token)
+                config.headers.Authorization = 'Bearer ' + Fluro.token;
+            }
+            return config;
+        },
+    };
+});
+
+
 
 
 

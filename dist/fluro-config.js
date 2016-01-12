65d7d7dbfdd64bfbb44c3cd02c333adc15f0bfbc
Cade <cade@hyperblaster.org>
2016-01-13 00:46:33 +1100

    Added Fluro Token Service

--- dist/fluro-config.js
+++ dist/fluro-config.js
@@ -33,24 +33,97 @@ angular.module('fluro.config', [])
 
     //////////////////////////
 
+    controller.recall = function() {
+        if (localStorage.session) {
+            $rootScope.user = localStorage.session;
+        }
+    }
+
+    //////////////////////////
+
     //Submit and send back the user
     controller.login = function(details, successCallback, errorCallback) {
-        $http.post(Fluro.apiURL + '/token/login', details)
-            .success(function(res) {
-                console.log('Token Success', res);
 
-                if (successCallback) {
-                    successCallback(res);
-                }
-            })
-            .error(function(res) {
-                console.log('Token Error', res);
-                if (errorCallback) {
-                    errorCallback(res);
-                }
-            });
+        var request = $http.post(Fluro.apiURL + '/token/login', details);
 
+        request.success(function(res) {
+            console.log('Token Success', res);
+            $rootScope.user = res;
+
+            if (details.remember) {
+                localStorage.session = res;
+            }
+
+            if (successCallback) {
+                successCallback(res);
+            }
+        });
+
+        request.error(function(res) {
+            console.log('Token Error', res);
+            if (errorCallback) {
+                errorCallback(res);
+            }
+        });
     };
+
+    //////////////////////////
+
+    controller.hasExpired = function() {
+        if (localStorage.session) {
+            var expiry = new Date(localStorage.session.expires);
+            var now = new Date();
+            return (expiry.getTime() <= now.getTime());
+        }
+    }
+
+    //////////////////////////
+
+    controller.refresh = function(successCallback, errorCallback) {
+
+        var session = localStorage.session;
+
+        if (session) {
+            if (session.refreshToken) {
+                var request = $http.post(Fluro.apiURL + '/token/refresh', {
+                    refreshToken: session.refreshToken
+                });
+
+                request.success(function(res) {
+                    console.log('Refresh Token Success', res);
+                    localStorage.session = res;
+                    $rootScope.user = res;
+
+                    if (successCallback) {
+                        successCallback(res);
+                    }
+                });
+
+                request.error(function(res) {
+                    console.log('Error refreshing token', res)
+                    controller.deleteSession();
+
+                    if (errorCallback) {
+                        errorCallback(res);
+                    }
+                });
+            }
+        }
+    }
+
+    //////////////////////////
+
+    controller.deleteSession = function() {
+        delete localStorage.session;
+        delete $rootScope.user;
+    }
+
+
+    //////////////////////////
+
+    controller.logout = function() {
+        controller.deleteSession();
+    }
 
     //////////////////////////
 
@@ -60,38 +133,66 @@ angular.module('fluro.config', [])
 
 
 
-.service('FluroAuthentication', function($q, Fluro) {
+.service('FluroAuthentication', function($q, Fluro, FluroTokenService) {
     return {
         'request': function(config) {
-            if (Fluro.token) {
-                config.headers.Authorization = 'Bearer ' + Fluro.token;
+
+            //Check if the token might expire
+            if (FluroTokenService.hasExpired()) {
+                console.log('token expired and requires refresh');
+                //Wait for a result
+                var deferred = $q.defer();
+
+
+                function refreshSuccess(res) {
+                    console.log('Refreshed successfully', res)
+                    deferred.resolve(config);
+                }
+
+                function refreshFailed(res) {
+                    console.log('Refresh failed', res)
+                    deferred.reject(config);
+                }
+
+                FluroTokenService.refresh(refreshSuccess, refreshFailed);
+
+                return deferred.promise;
+
+            } else {
+
+                //Do the normal thing
+
+                if (Fluro.token) {
+                    config.headers.Authorization = 'Bearer ' + Fluro.token;
+                }
+
+                ////////////////////////////////////////
+
+                var date = new Date();
+
+                if (Fluro.timezoneOffset && String(Fluro.timezoneOffset).length) {
+
+                    //Localized time
+                    var websiteOffset = Fluro.timezoneOffset; // * 60;
+                    var viewerOffset = (date.getTimezoneOffset() * -1);
+
+                    ///////////////////////////////////////////////////////////////
+
+                    //Get the difference
+                    var hoursDifference = websiteOffset - viewerOffset;
+                    var offsetDifference = hoursDifference * 60 * 1000;
+
+                    //Adjust the date
+                    date.setTime(date.getTime() + offsetDifference);
+                }
+
+                config.headers['fluro-request-date'] = date.toUTCString();
+
+                return config;
+
             }
 
-            ////////////////////////////////////////
 
-            var date = new Date();
-
-            if (Fluro.timezoneOffset && String(Fluro.timezoneOffset).length) {
-
-                //Localized time
-                var websiteOffset = Fluro.timezoneOffset; // * 60;
-                var viewerOffset = (date.getTimezoneOffset() * -1);
-
-                ///////////////////////////////////////////////////////////////
-
-                //Get the difference
-                var hoursDifference = websiteOffset - viewerOffset;
-                var offsetDifference = hoursDifference * 60 * 1000;
-
-                //Adjust the date
-                date.setTime(date.getTime() + offsetDifference);
-            }
-
-            config.headers['fluro-request-date'] = date.toUTCString();
-
-            ////////////////////////////////////////
-
-            return config;
         },
     };
 });
